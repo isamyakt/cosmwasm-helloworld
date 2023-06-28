6aa7996ad31ccc19b3b08a64c8cf8a99ea1a15bc
isamyakt <isamyakt17@gmail.com>
2023-06-28 15:20:04 +0530

    implemented Clone 🛠

--- src/msg.rs
+++ src/msg.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct GreetResp {
     pub message: String,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub enum QueryMsg {
     Greet {},
 }
