222a37feb4b6a6ca68753e1534b975cfc32b9742
isamyakt <isamyakt17@gmail.com>
2023-06-28 15:19:19 +0530

    contract entrypoint 📝

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,47 @@
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    entry_point, 
+    Binary, 
+    Deps, 
+    DepsMut, 
+    Empty, 
+    Env, 
+    MessageInfo, 
+    Response, 
+    StdResult,
 };
 
 mod contract;
 mod msg;
 
 #[entry_point]
-pub fn instantiate(deps: DepsMut, env: Env, info: MessageInfo, msg: Empty)
-  -> StdResult<Response>
-{
-    contract::instantiate(deps, env, info, msg)
+pub fn instantiate(
+    deps: DepsMut, 
+    env: Env, 
+    info: MessageInfo, 
+    msg: Empty
+) -> StdResult<Response> {
+
+    contract::instantiate(
+        deps, 
+        env, 
+        info, 
+        msg
+    )
 }
 
 #[entry_point]
-pub fn query(deps: Deps, env: Env, msg: msg::QueryMsg)
-  -> StdResult<Binary>
-{
-    contract::query(deps, env, msg)
+pub fn query(
+    deps: Deps, 
+    env: Env, 
+    msg: msg::QueryMsg
+) -> StdResult<Binary> {
+    
+    contract::query(
+        deps, 
+        env, 
+        msg
+    )
+
 }
 
 // cargo wasm or cargo wasm-debug
